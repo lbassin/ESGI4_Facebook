20f73aee3c54c62e67cad16c84b7d7d46949e31e
Bassin Laurent <laurent@bassin.info>
2017-12-08 16:48:01 +0100

    Check Missing Permissions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,14 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts/app')
 
-        <title>Foliobook</title>
+@section('title', 'Foliobook')
 
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-        <script src="//cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-    </head>
-    <body>
-        <div id="app"></div>
-        <div id="particles-js"></div>
+@section('content')
+    <div id="app"></div>
+    <div id="particles-js"></div>
 
-        <span id="fbConnect" class="pulse-button">Créer mon site</span>
+    <span id="fbConnect" class="pulse-button">Créer mon site</span>
 
-        <script>
-            window.fbData = {
-                appId: '{{ env('FACEBOOK_APP_ID') }}',
-                scope: 'public_profile,email'
-            };
-
-            window.URLs = {
-                dashboard: '{{ route('dashboard') }}'
-            }
-        </script>
-
-        <script src="{{ asset('js/fb-login.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('js/particles.js') }}"></script>
-    </body>
-</html>
+    <script src="{{ asset('js/fb-login.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/particles.js') }}"></script>
+@endsection
---
+++ resources/views/reAskPermissions.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/app')
+
+@section('title', 'Facebook Permissions')
+
+@section('content')
+    <h1 style="color: #fff;">Permissions</h1>
+@endsection
