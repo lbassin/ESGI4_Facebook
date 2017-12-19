b3dec44745eef5ea6a26bc8f42cdf87f2a28adb6
xRaz <maxime.marquet1@gmail.com>
2017-12-19 09:11:26 +0100

    fix dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,55 +19,109 @@
         <div class="select-dashboard">
             <div class="select-choice">
                 <h1>Choisissez votre site</h1>
-                <select name="sources" id="sources" class="custom-select sources" placeholder="Source Type">
-                    <option value="profile">Profile</option>
-                    <option value="word">Word</option>
-                    <option value="hashtag">Hashtag</option>
-                    <option value="hashtag">Hashtag</option>
-                    <option value="hashtag">Hashtag</option>
+                <select class="">
+                    <option value="" data-display-text="Fruits">None</option>
+                    <option value="apples" disabled>Apples</option>
+                    <option value="bananas">Bananas</option>
+                    <option value="oranges">Oranges</option>
+                    <option value="oranges">Oranges</option>
+                    <option value="oranges">Oranges</option>
+                    <option value="oranges">Oranges</option>
+                    <option value="oranges">Oranges</option>
                 </select>
+                <div class="add-page">+</div>
             </div>
         </div>
     </div>
 
 
     <script>
+function create_custom_dropdowns() {
+  $('select').each(function(i, select) {
+    if (!$(this).next().hasClass('dropdown')) {
+      $(this).after('<div class="dropdown ' + ($(this).attr('class') || '') + '" tabindex="0"><span class="current"></span><div class="list"><ul></ul></div></div>');
+      var dropdown = $(this).next();
+      var options = $(select).find('option');
+      var selected = $(this).find('option:selected');
+      dropdown.find('.current').html(selected.data('display-text') || selected.text());
+      options.each(function(j, o) {
+        var display = $(o).data('display-text') || '';
+        dropdown.find('ul').append('<li class="option ' + ($(o).is(':selected') ? 'selected' : '') + '" data-value="' + $(o).val() + '" data-display-text="' + display + '">' + $(o).text() + '</li>');
+      });
+    }
+  });
+}
 
-        $(".custom-select").each(function () {
-            var classes = $(this).attr("class"),
-                id = $(this).attr("id"),
-                name = $(this).attr("name");
-            var template = '<div class="' + classes + '">';
-            template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
-            template += '<div class="custom-options">';
-            $(this).find("option").each(function () {
-                template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
-            });
-            template += '</div></div>';
+// Event listeners
 
-            $(this).wrap('<div class="custom-select-wrapper"></div>');
-            $(this).hide();
-            $(this).after(template);
-        });
-        $(".custom-option:first-of-type").hover(function () {
-            $(this).parents(".custom-options").addClass("option-hover");
-        }, function () {
-            $(this).parents(".custom-options").removeClass("option-hover");
-        });
-        $(".custom-select-trigger").on("click", function () {
-            $('html').one('click', function () {
-                $(".custom-select").removeClass("opened");
-            });
-            $(this).parents(".custom-select").toggleClass("opened");
-            event.stopPropagation();
-        });
-        $(".custom-option").on("click", function () {
-            $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
-            $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
-            $(this).addClass("selection");
-            $(this).parents(".custom-select").removeClass("opened");
-            $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
-        });
+// Open/close
+$(document).on('click', '.dropdown', function(event) {
+  $('.dropdown').not($(this)).removeClass('open');
+  $(this).toggleClass('open');
+  if ($(this).hasClass('open')) {
+    $(this).find('.option').attr('tabindex', 0);
+    $(this).find('.selected').focus();
+  } else {
+    $(this).find('.option').removeAttr('tabindex');
+    $(this).focus();
+  }
+});
+// Close when clicking outside
+$(document).on('click', function(event) {
+  if ($(event.target).closest('.dropdown').length === 0) {
+    $('.dropdown').removeClass('open');
+    $('.dropdown .option').removeAttr('tabindex');
+  }
+  event.stopPropagation();
+});
+// Option click
+$(document).on('click', '.dropdown .option', function(event) {
+  $(this).closest('.list').find('.selected').removeClass('selected');
+  $(this).addClass('selected');
+  var text = $(this).data('display-text') || $(this).text();
+  $(this).closest('.dropdown').find('.current').text(text);
+  $(this).closest('.dropdown').prev('select').val($(this).data('value')).trigger('change');
+});
 
+// Keyboard events
+$(document).on('keydown', '.dropdown', function(event) {
+  var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
+  // Space or Enter
+  if (event.keyCode == 32 || event.keyCode == 13) {
+    if ($(this).hasClass('open')) {
+      focused_option.trigger('click');
+    } else {
+      $(this).trigger('click');
+    }
+    return false;
+    // Down
+  } else if (event.keyCode == 40) {
+    if (!$(this).hasClass('open')) {
+      $(this).trigger('click');
+    } else {
+      focused_option.next().focus();
+    }
+    return false;
+    // Up
+  } else if (event.keyCode == 38) {
+    if (!$(this).hasClass('open')) {
+      $(this).trigger('click');
+    } else {
+      var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
+      focused_option.prev().focus();
+    }
+    return false;
+  // Esc
+  } else if (event.keyCode == 27) {
+    if ($(this).hasClass('open')) {
+      $(this).trigger('click');
+    }
+    return false;
+  }
+});
+
+$(document).ready(function() {
+  create_custom_dropdowns();
+});
     </script>
 @endsection
