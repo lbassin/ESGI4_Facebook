0b42345e6ffb91e0f03f9be48c70b33e7c538161
xRaz <maxime.marquet1@gmail.com>
2017-12-20 12:52:10 +0100

    permissions

--- resources/assets/js/fb/fb-login.js
+++ resources/assets/js/fb/fb-login.js
@@ -3,24 +3,24 @@ window.fbLoaded = function () {
 };
 
 function addFacebookLoginEvent() {
-    $('#spin').click(() => {
+    $('.spin').click(() => {
         var target = $(this).attr("id");
-        if ($("#spin").hasClass("done")) {
+        if ($(".spin").hasClass("done")) {
             // Do nothing
         } else {
-            $("#spin").addClass("processing");
+            $(".spin").addClass("processing");
             FB.login(function (result) {
                 if (result.status === 'connected') {
                     setTimeout(function () {
-                        $("#spin").removeClass("processing");
-                        $("#spin").addClass("done");
+                        $(".spin").removeClass("processing");
+                        $(".spin").addClass("done");
                         setTimeout(function () {
                             window.location.href = window.URLs.dashboard;
                         }, 500);
                     }, 1500);
                 } else {
-                    $("#spin").removeClass("processing");
-                    $("#spin").addClass("spin");
+                    $(".spin").removeClass("processing");
+                    $(".spin").addClass("spin");
                 }
             }, {
                 scope: window.fbData.scope
