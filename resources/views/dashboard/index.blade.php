7e7d3fb330e34a3c68d163814622cb64d4c09562
Bassin Laurent <laurent@bassin.info>
2017-12-21 18:48:14 +0100

    Refactor select js

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -18,163 +18,179 @@
 
         <div class="select-dashboard">
             <div class="select-choice">
-				<div class="select-title">
-                	<h1>Choisissez votre site</h1>
-				</div>
-				<div class="select-action">
-					<select class="">
-						<option value="" data-display-text="Fruits">None</option>
-						<option value="apples" disabled>Apples</option>
-						<option value="bananas">Bananas</option>
-						<option value="oranges">Oranges</option>
-						<option value="oranges">Oranges</option>
-						<option value="oranges">Oranges</option>
-						<option value="oranges">Oranges</option>
-						<option value="oranges">Oranges</option>
-					</select>
-					<div class="add-page">
-						<i class="fa fa-plus" aria-hidden="true"></i>
-					</div>
-				</div>
+                <div class="select-title">
+                    <h1>Choisissez votre site</h1>
+                </div>
+                <div class="select-action">
+                    <select id="website-select">
+                        <option value="" disabled selected>Website !</option>
+                        @foreach($websites as $website)
+                            <option value="{{ $website['id'] }}">{{ $website['name'] }}</option>
+                        @endforeach
+                    </select>
+                    <div class="add-page">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
     <div class="md-modal md-effect-12">
         <div class="md-content">
-			<h1>NOUVEAU SITE</h1>
-			<h2>Choisissez la page à synchroniser</h2>
-			<ul>
-				@foreach($pages as $page)
-					<li>
-						<a href="{{ route('dashboard.new', ['id' => $page['id']]) }}">
-						<div>
-							<img src="{{ $page['picture']['data']['url'] }}" alt="">
-							<span>{{ $page['name'] }}</span>
-						</div>
-						</a>
-					</li>
-				@endforeach
-      		</ul>
+            <h1>NOUVEAU SITE</h1>
+            <h2>Choisissez la page à synchroniser</h2>
+            <ul>
+                @foreach($pages as $page)
+                    <li>
+                        <a href="{{ route('dashboard.new', ['id' => $page['id']]) }}">
+                            <div>
+                                <img src="{{ $page['picture']['data']['url'] }}" alt="">
+                                <span>{{ $page['name'] }}</span>
+                            </div>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 
     <div class="md-overlay">
         <button class="md-close">
-			<i class="fa fa-times" aria-hidden="true"></i>
-		</button>
+            <i class="fa fa-times" aria-hidden="true"></i>
+        </button>
     </div>
 
-
     <script>
-function create_custom_dropdowns() {
-  $('select').each(function(i, select) {
-    if (!$(this).next().hasClass('dropdown')) {
-      $(this).after('<div class="dropdown ' + ($(this).attr('class') || '') + '" tabindex="0"><span class="current"></span><div class="list"><ul></ul></div></div>');
-      var dropdown = $(this).next();
-      var options = $(select).find('option');
-      var selected = $(this).find('option:selected');
-      dropdown.find('.current').html(selected.data('display-text') || selected.text());
-      options.each(function(j, o) {
-        var display = $(o).data('display-text') || '';
-        dropdown.find('ul').append('<li class="option ' + ($(o).is(':selected') ? 'selected' : '') + '" data-value="' + $(o).val() + '" data-display-text="' + display + '">' + $(o).text() + '</li>');
-      });
-    }
-  });
-}
+        function initDropdown() {
+            let select = $('#website-select');
+            let dropdownDiv = $('<div>').addClass('dropdown');
+            let currentSpan = $('<span>').addClass('current');
+            let listDiv = $('<div>').addClass('list');
+            let optionsList = $('<ul>');
 
-// Event listeners
+            let options = $(select).find('option');
+            let selectedText = $(select).find('option:selected').text();
 
-// Open/close
-$(document).on('click', '.dropdown', function(event) {
-  $('.dropdown').not($(this)).removeClass('open');
-  $(this).toggleClass('open');
-  if ($(this).hasClass('open')) {
-    $(this).find('.option').attr('tabindex', 0);
-    $(this).find('.selected').focus();
-  } else {
-    $(this).find('.option').removeAttr('tabindex');
-    $(this).focus();
-  }
-});
-// Close when clicking outside
-$(document).on('click', function(event) {
-  if ($(event.target).closest('.dropdown').length === 0) {
-    $('.dropdown').removeClass('open');
-    $('.dropdown .option').removeAttr('tabindex');
-  }
-  event.stopPropagation();
-});
-// Option click
-$(document).on('click', '.dropdown .option', function(event) {
-  $(this).closest('.list').find('.selected').removeClass('selected');
-  $(this).addClass('selected');
-  var text = $(this).data('display-text') || $(this).text();
-  $(this).closest('.dropdown').find('.current').text(text);
-  $(this).closest('.dropdown').prev('select').val($(this).data('value')).trigger('change');
-});
+            listDiv.append(optionsList);
+            currentSpan.text(selectedText);
+            dropdownDiv.append(currentSpan).append(listDiv);
 
-// Keyboard events
-$(document).on('keydown', '.dropdown', function(event) {
-  var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
-  // Space or Enter
-  if (event.keyCode == 32 || event.keyCode == 13) {
-    if ($(this).hasClass('open')) {
-      focused_option.trigger('click');
-    } else {
-      $(this).trigger('click');
-    }
-    return false;
-    // Down
-  } else if (event.keyCode == 40) {
-    if (!$(this).hasClass('open')) {
-      $(this).trigger('click');
-    } else {
-      focused_option.next().focus();
-    }
-    return false;
-    // Up
-  } else if (event.keyCode == 38) {
-    if (!$(this).hasClass('open')) {
-      $(this).trigger('click');
-    } else {
-      var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
-      focused_option.prev().focus();
-    }
-    return false;
-  // Esc
-  } else if (event.keyCode == 27) {
-    if ($(this).hasClass('open')) {
-      $(this).trigger('click');
-    }
-    return false;
-  }
-});
+            $(select).after(dropdownDiv);
 
-$(document).ready(function() {
-  create_custom_dropdowns();
-});
+            options.each(function (index, option) {
+                option = $(option);
 
+                let listItem = $('<li>').addClass('option');
+                listItem.val(option.val());
+                listItem.text(option.text());
 
-////////
-////////
-////////
-////////
+                if (option.is(':selected')) {
+                    listItem.addClass('selected');
+                }
 
-$(function () {
-  
-	$('.add-page').on('click', function() {
-		$('.md-modal').addClass('md-show');
-	});
+                if (option.attr('disabled')) {
+                    listItem.addClass('disabled');
+                }
 
-	$(document).on('keydown', function(event) {
-		if (event.keyCode == 27) {
-			$('.md-modal').removeClass('md-show');
-		}
-	});
-	$('.md-close').on('click', function() {
-		$('.md-modal').removeClass('md-show');
-	});
-});
+                optionsList.append(listItem);
+            });
+
+            addListener();
+        }
+
+        function addListener() {
+            let dropdown = $('.dropdown');
+
+            dropdown.on('click', function () {
+                let dropdown = $(this);
+
+                dropdown.toggleClass('open');
+                if (dropdown.hasClass('open')) {
+                    // Allows keyboard events
+                    dropdown.find('.option').attr('tabindex', 0);
+                    dropdown.find('.selected').focus();
+                } else {
+                    dropdown.find('.option').removeAttr('tabindex');
+                    dropdown.focus();
+                }
+            });
+
+            $(document).on('click', function (event) {
+                if ($(event.target).closest('.dropdown').length === 0) {
+                    $('.dropdown').removeClass('open');
+                    $('.dropdown .option').removeAttr('tabindex');
+
+                    event.stopPropagation();
+                }
+            });
+
+            $('.dropdown .option').on('click', function () {
+                let selected = $(this);
+                let dropdown = selected.closest('.dropdown');
+
+                selected.closest('.list').find('.selected').removeClass('selected');
+                selected.addClass('selected');
+
+                dropdown.find('.current').text(selected.text());
+                dropdown.prev('select').val(selected.data('value')).trigger('change');
+            });
+
+            dropdown.on('keydown', function (event) {
+                let focusOption = $(this).find('.list .option:focus')[0];
+
+                if (!focusOption) {
+                    focusOption = $(this).find('.list .option.selected')[0]
+                }
+
+                switch (event.keyCode) {
+                    case 32: // Space
+                    case 13: // Enter
+                        if ($(this).hasClass('open')) {
+                            $(focusOption).trigger('click');
+                        } else {
+                            $(this).trigger('click');
+                        }
+                        break;
+                    case 40: // Down
+                        if (!$(this).hasClass('open')) {
+                            $(this).trigger('click');
+                        } else {
+                            $(focusOption).next().focus();
+                        }
+                        break;
+                    case 38: // Up
+                        if (!$(this).hasClass('open')) {
+                            $(this).trigger('click');
+                        } else {
+                            $(focusOption).prev().focus();
+                        }
+                        break;
+                    case 27: // Echap
+                        if ($(this).hasClass('open')) {
+                            $(this).trigger('click');
+                        }
+                        break;
+                }
+            });
+        }
+
+        $(function () {
+            $('.add-page').on('click', function () {
+                $('.md-modal').addClass('md-show');
+            });
+
+            $(document).on('keydown', function (event) {
+                if (event.keyCode === 27) {
+                    $('.md-modal').removeClass('md-show');
+                }
+            });
+
+            $('.md-close').on('click', function () {
+                $('.md-modal').removeClass('md-show');
+            });
+
+            initDropdown();
+        });
     </script>
 @endsection
