59cd84253749e8ecfe1902f853e2e7ea7b19f29e
Bassin Laurent <laurent@bassin.info>
2017-12-21 18:51:05 +0100

    Remove VueJS

--- public/js/fb-reAskPermissions.js
+++ public/js/fb-reAskPermissions.js
@@ -60,20 +60,20 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 39);
+/******/ 	return __webpack_require__(__webpack_require__.s = 35);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 39:
+/***/ 35:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(40);
+module.exports = __webpack_require__(36);
 
 
 /***/ }),
 
-/***/ 40:
+/***/ 36:
 /***/ (function(module, exports) {
 
 window.fbLoaded = function () {
