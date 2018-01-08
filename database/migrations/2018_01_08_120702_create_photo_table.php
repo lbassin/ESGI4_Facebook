3f2ca419145268536a03263c29dcd44eded3998a
Bassin Laurent <laurent@bassin.info>
2018-01-08 13:11:15 +0100

    Add photo migration

---
+++ database/migrations/2018_01_08_120702_create_photo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo', function (Blueprint $table) {
+            $table->bigInteger(\App\Model\Photo::ID);
+            $table->bigInteger(\App\Model\Photo::ALBUM_ID);
+            $table->boolean(\App\Model\Photo::VISIBLE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo');
+    }
+}
