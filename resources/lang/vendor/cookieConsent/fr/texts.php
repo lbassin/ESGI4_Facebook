1ab7e88148016c697f647cf2df4b4a2cd11267d1
Bassin Laurent <laurent@bassin.info>
2018-01-13 23:18:09 +0100

    Set cookie consent messag

--- resources/lang/vendor/cookieConsent/fr/texts.php
+++ resources/lang/vendor/cookieConsent/fr/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => "En continuant à naviguer sur notre site, vous acceptez l'utilisation de cookies.",
+    'message' => "En continuant à naviguer sur notre site, vous acceptez l'utilisation de cookies. <a href='/policy'>En savoir plus.</a>",
     'agree' => 'Accepter',
 ];
