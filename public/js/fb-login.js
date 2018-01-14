5f8724946298e22750467450b6aed268cf9fa3dc
Bassin Laurent <laurent@bassin.info>
2018-01-14 22:48:17 +0100

    Add login action

--- public/js/fb-login.js
+++ public/js/fb-login.js
@@ -81,31 +81,16 @@ window.fbLoaded = function () {
 };
 
 function addFacebookLoginEvent() {
-    var _this = this;
-
-    $('.spin').click(function () {
-        var target = $(_this).attr("id");
-        if ($(".spin").hasClass("done")) {
-            // Do nothing
-        } else {
-            $(".spin").addClass("processing");
-            FB.login(function (result) {
-                if (result.status === 'connected') {
-                    setTimeout(function () {
-                        $(".spin").removeClass("processing");
-                        $(".spin").addClass("done");
-                        setTimeout(function () {
-                            window.location.href = window.URLs.dashboard;
-                        }, 500);
-                    }, 1500);
-                } else {
-                    $(".spin").removeClass("processing");
-                    $(".spin").addClass("spin");
-                }
-            }, {
-                scope: window.fbData.scope
-            });
-        }
+    $('.login-fb').click(function () {
+        FB.login(function (result) {
+            if (result.status === 'connected') {
+                window.location.href = window.URLs.dashboard;
+            } else {
+                alert('Error');
+            }
+        }, {
+            scope: window.fbData.scope
+        });
     });
 }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                 <a href="#">Pricing</a>
                 <a href="#">Docs</a>
                 <a href="#">Support</a>
-                <a class="primary" href="#">Get started</a>
+                <a class="primary fb-login-open" href="#">Get started</a>
             </nav>
 
             <div class="content">
@@ -29,7 +29,7 @@
                     <div>
                         <p>Construisez un site web qui vous correspond</p>
                     </div>
-                    <button>Créez votre site</button>
+                    <button class="fb-login-open">Créez votre site</button>
                 </div>
                 <div class="image">
                     <img src="{{ asset('images/home.jpg') }}" alt="">
@@ -37,6 +37,19 @@
             </div>
         </div>
     </div>
+
+    <div class="login-modal">
+        <div class="close-modal fb-login-close"><i class="fa fa-times" aria-hidden="true"></i></div>
+        <h3>Créez votre site web</h3>
+        <p>
+            Veuillez vous connectez à l'aide de votre compte facebook afin de synchroniser vos images
+        </p>
+        <button class="login-fb"><i class="fa fa-facebook" aria-hidden="true"></i> Continuer avec Facebook</button>
+        <p class="legend">
+            Nous ne publierons rien sur votre profil Facebook
+        </p>
+    </div>
+    <div class="login-modal-overlay fb-login-close"></div>
 
     <script>
         $(document).ready(function () {
@@ -63,6 +76,19 @@
                 });
             }, 1500);
         });
+
+        $('.fb-login-close').click(closeModal);
+        $('.fb-login-open').click(openModal);
+
+        function openModal(){
+            $('.login-modal').fadeIn();
+            $('.login-modal-overlay').fadeIn();
+        }
+
+        function closeModal(){
+            $('.login-modal').fadeOut();
+            $('.login-modal-overlay').fadeOut();
+        }
     </script>
 
     <script src="{{ asset('js/app.js') }}"></script>
