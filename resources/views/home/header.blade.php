e00ce43926bdd19851b9460672b478d3c1a4d04e
xRaz <maxime.marquet1@gmail.com>
2018-01-15 10:55:45 +0100

    policy

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="logo">
+        Wawat
+    </div>
+    <a href="#">Pricing</a>
+    <a href="#">Docs</a>
+    <a href="#">Support</a>
+    <a class="primary fb-login-open" href="#">Get started</a>
+</nav>
